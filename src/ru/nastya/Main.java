e1b0ed4ccfa15852efb7fd26e83da3bcd7a2adf8
nastya <Nastay2390@gmail.com>
2019-01-27 13:24:51 +0300

    console program

--- src/ru/nastya/Main.java
+++ src/ru/nastya/Main.java
@@ -2,69 +2,61 @@ package ru.nastya;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.function.ToIntFunction;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
         int num, input;
 
-        do {
-            do {
-                System.out.println("Please, enter number between 1 and 3999, for exit press 0:");
-                try {
-                    num = in.nextInt();
-                }catch (InputMismatchException e){
+        if(args.length != 1) {
+            System.out.println("To use this tool you should enter one parameter");
+            return;
+        }else{
+            try {
+                    num = Integer.parseInt(args[0]);
+                }catch (NumberFormatException e){
                     System.out.println("Incorrect input. Try again");
-                    num = -1;
-                    in.nextLine();
-                    continue;
+                    return;
                 }
 
-                if (num == 0) {
-                    break;
+            if(num > 0 & num < 4000) {
+                input = num;
+
+                RomainanNumber romN = new RomainanNumber(num);
+
+                // тысячи
+                switch (num / 1000) {
+                    case 1:
+                        romN.setRes("M");
+                        break;
+                    case 2:
+                        romN.setRes("MM");
+                        break;
+                    case 3:
+                        romN.setRes("MMM");
+                        break;
                 }
-            } while (num < 1 || num > 3999);
 
-            if (num == 0) {
-                break;
+                num = num - (num / 1000) * 1000;
+
+                // сотые
+                romN.getResults("C", "D", "M", num / 100);
+                num = num - (num / 100) * 100;
+
+                // десятки
+                romN.getResults("X", "L", "C", num / 10);
+                num = num - (num / 10) * 10;
+
+                // единицы
+                romN.getResults("I", "V", "X", num % 10);
+
+                System.out.println("You enter " + input + " in Romanian letters = " + romN.resStr() + "\n");
+            }else{
+                System.out.println("To use this tool you should enter the number between 1 ... 3999");
             }
-            input = num;
-
-            RomainanNumber romN = new RomainanNumber(num);
-
-            // тысячи
-            switch (num / 1000) {
-                case 1:
-                    romN.setRes("M");
-                    break;
-                case 2:
-                    romN.setRes("MM");
-                    break;
-                case 3:
-                    romN.setRes("MMM");
-                    break;
-            }
-
-            num = num - (num / 1000) * 1000;
-
-            // сотые
-            romN.getResults("C", "D", "M", num / 100);
-            num = num - (num / 100) * 100;
-
-            // десятки
-            romN.getResults("X", "L", "C", num / 10);
-            num = num - (num / 10) * 10;
-
-            // единицы
-            romN.getResults("I", "V", "X", num % 10);
-
-            System.out.println("You enter " + input + " in Romanian letters = " + romN.resStr() + "\n");
-        } while (num != 0);
-
-        System.out.println("Bye");
-        in.close();
+        }
 
     }
 }
